419eb5435f8a302e491d72198aa0396997ba1261
Dilshodbek <92425517+KhamidovDilshod@users.noreply.github.com>
2023-07-10 15:56:08 +0500

    Adding multiple settings file based on current environment, Unfinished

--- Services/Api-Gateway/Program.cs
+++ Services/Api-Gateway/Program.cs
@@ -1,9 +1,19 @@
+using Authentication.Exceptions;
+using Authentication.Extensions;
+using Shared.Serilog;
+using Shared.Swagger;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder
+    .AddCustomLogging();
+
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 var app = builder.Build();
 
 app.MapReverseProxy();
+app.UseMiddleware<ErrorHandlerMiddleware>();
 app.MapGet("/", () => "Api gateway");
 
 
---
+++ Services/Common/Configuration/ConfigurationExtension.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Shared.Configuration;
+
+public static class ConfigurationExtension
+{
+    private static string _env = "dev";
+
+    public static WebApplicationBuilder AddConfiguration(this WebApplicationBuilder builder)
+    {
+        try
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+            _env = (config.GetSection("Environment").Value ??
+                    Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"))!;
+            
+        }
+        catch (Exception)
+        {
+            _env = "dev";
+            Console.WriteLine($"Couldn't read Environment or appsettings.{_env}.json");
+        }
+        // var app=builder
+
+        return builder;
+    }
+}
