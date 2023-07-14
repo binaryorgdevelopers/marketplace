06b6cfeb75449f36d2a916b4fc22bdd222167d26
Dilshodbek <khamidovdilshodbekk@gmail.com>
2023-07-14 07:54:28 +0500

    Some changes

--- Services/Notification.Client/script.js
+++ Services/Notification.Client/script.js
@@ -8,8 +8,8 @@ const connection = await tryConnect().catch((er) => console.log(er));
 connection.onclose(async () => {
     main.style.border = '1px solid #FF5722'
     showMessage('Connection closed by server', 'danger', 'border-danger')
-    await tryConnect().then(c=>{
-    showMessage('Reconnecting', 'success', 'border-success')
+    await tryConnect().then(c => {
+        showMessage('Reconnecting', 'success', 'border-success')
     });
 })
 connection.on('Initialized', (msg) => showMessage(msg, 'success', 'border-success'));
@@ -24,3 +24,18 @@ function pCreator(innerText, classname = null, id = null) {
     element.innerText = innerText;
     return element
 }
+
+function RenderImage(val) {
+    const img = document.createElement('img',)
+    img.setAttribute('src', val);
+    content.appendChild(img);
+}
+function RenderVideo(val){
+    const img = document.createElement('video',)
+    img.setAttribute('src', val);
+    img.setAttribute('type','video/mp4')
+    content.appendChild(img);
+
+}
+RenderImage('http://localhost:5203/file/image/images.png')
+// RenderVideo('http://localhost:5203/file/video/okmIEVj8D8ANUqQgERrnQsQfrHuEiekqSB6bBM.mp4')
